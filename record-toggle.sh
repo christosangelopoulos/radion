7a209d0ab2c5b909ad2a86dabed67184fbbed555
Christos Angelopoulos <christakis2020@yandex.com>
2023-10-27 02:19:38 +0300

    improve record script, yad management

--- record-toggle.sh
+++ record-toggle.sh
@@ -4,7 +4,7 @@ function name_save ()
 {
 	if [[ $YAD_TOGGLE == "yes" ]]
 	then
-	yad --image="$HOME/.cache/radion/png/stop.png" --text="Recording Stopped..." --no-buttons --on-top --undecorated --no-focus --skip-taskbar --sticky --geometry=${YAD_WIDTH}x${YAD_HEIGHT}+$YAD_X+$YAD_Y --borders=10;fi &
+	yad --image="$HOME/.cache/radion/png/stop.png" --text="Recording Stopped..." --no-buttons --on-top --undecorated --no-focus --skip-taskbar --sticky --geometry=${YAD_WIDTH}x${YAD_HEIGHT}+$YAD_X+$YAD_Y --borders=10&YAD_PID=$(pidof yad |awk '{print $1}')&&	echo $YAD_PID>/tmp/rec-yad-pid.txt;echo "yad pid:"$YAD_PID;fi &
 	case $REC_NAME_PROTOCOL in
 		"date")REC_NAME="$(date +%Y-%m-%d\_%T)";
 		;;
@@ -14,24 +14,23 @@ function name_save ()
 		;;
 		"blank")if [[ $YAD_TOGGLE == "yes" ]];then yad_name_file "Please enter file name:" "";else REC_NAME="$(date +%s)";fi
 	esac
-	sox /tmp/radion-tmp1.wav "$HOME""$RECORD_DIR""$REC_NAME.$OUT_FORMAT" norm
-	if [[ $YAD_TOGGLE == "yes" ]];then killall yad
+	sox /tmp/radion-tmp1.wav "$HOME""$RECORD_DIR""$REC_NAME.$OUT_FORMAT" norm&&	if [[ $YAD_TOGGLE == "yes" ]];then kill $(cat /tmp/rec-yad-pid.txt)
 	yad --image="$HOME/.cache/radion/png/audio.png" --text="$REC_NAME.$OUT_FORMAT normalized and saved." --button=gtk-ok:0 --undecorated --on-top --no-focus --skip-taskbar --sticky --geometry=${YAD_WIDTH}x${YAD_HEIGHT}+$YAD_X+$YAD_Y --borders=10 --timeout="$YAD_DURATION";fi;
 }
 
-function toggle_on ()
+function rec_toggle_on ()
 {
 	if [[ $YAD_TOGGLE == "yes" ]]
 	then
-		killall yad
-		yad --image="$HOME/.cache/radion/png/record.png" --text="Recording..." --no-buttons --undecorated --no-focus --on-top --skip-taskbar --sticky --geometry=${YAD_WIDTH}x${YAD_HEIGHT}+$YAD_X+$YAD_Y --borders=10
-	fi&	rec -c 2 -r 44100 /tmp/radion-tmp1.wav
+#killall yad
+		yad --image="$HOME/.cache/radion/png/record.png" --text="Recording..." --no-buttons --undecorated --no-focus --on-top --skip-taskbar --sticky --geometry=${YAD_WIDTH}x${YAD_HEIGHT}+$YAD_X+$YAD_Y --borders=10&YAD_PID=$(pidof yad |awk '{print $1}')&&	echo $YAD_PID>/tmp/rec-yad-pid.txt;echo "yad pid:"$YAD_PID
+	fi
+	rec -c 2 -r 44100 /tmp/radion-tmp1.wav
 }
 
-function toggle_off ()
+function rec_toggle_off ()
 {
-	kill $REC_PID
-	if [[ $YAD_TOGGLE == "yes" ]];then killall yad;fi
+	kill $REC_PID&&	if [[ $YAD_TOGGLE == "yes" ]];then kill $(cat /tmp/rec-yad-pid.txt);fi
 }
 
 function yad_name_file()
@@ -48,7 +47,7 @@ function yad_name_file()
 			--window-icon=$HOME/.cache/radion/png/audio.png)"
 	if [[ $? -eq 1 ]]
 	then
-		if [[ $YAD_TOGGLE == "yes" ]];then killall yad;fi
+		if [[ $YAD_TOGGLE == "yes" ]];then kill $(cat /tmp/rec-yad-pid.txt);fi
 		exit
 	fi
 	if [[ -z $REC_NAME ]];then REC_NAME="$(date +%s)";fi
@@ -70,8 +69,8 @@ function load_config ()
 load_config
 REC_PID=$(pidof rec)
 if [[ -n $REC_PID ]]
-then toggle_off
+then rec_toggle_off
 else
-	toggle_on
+	rec_toggle_on
 	name_save
 fi
