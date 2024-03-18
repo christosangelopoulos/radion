5eeb39eb4d28528b927479af1adfdce928810e8a
Christos Angelopoulos <christakis2020@yandex.com>
2024-03-18 20:59:39 +0200

    install script now mac compatible

--- install.sh
+++ install.sh
@@ -1,42 +1,37 @@
-#!/usr/bin/env bash
-
-preserve() {
-  local SRC=$1
-  local DST=$2
-  test ! -f ${DST}/${SRC} && install -Dvcm644 -b ${SRC} -t ${DST}
-}
+#!/bin/bash
 
 uninstall() {
-    rm -vf ~/.local/bin/radion.sh
-    rm -vf ~/.local/bin/record-toggle.sh
-    rm ~/.config/mpv/scripts/icecast-logger.lua
-    rm ~/.config/mpv/icyhistory.log
+    rm -vf "$HOME"/.local/bin/radion.sh
+    rm -vf "$HOME"/.local/bin/record-toggle.sh
+    rm "$HOME"/.config/mpv/scripts/icecast-logger.lua
+    rm "$HOME"/.config/mpv/icyhistory.log
 }
 
 case $1 in
-  install)
-    preserve stations.txt ~/.config/radion
-    preserve radion.conf ~/.config/radion
-    install -Dvcm644 -b icecast-logger.lua -t ~/.config/mpv/scripts/
-    install -Dvcm644 -b radion-rofi-theme.rasi -t ~/.config/radion/
-    install -Dvcm755 -d ~/Music/radion/
-    install -Dvcm755 radion.sh ~/.local/bin/
-    install -Dvcm755 record-toggle.sh ~/.local/bin/
-    install -Dvcm644 png/* -t ~/.config/radion/png
-    ;;
-  uninstall)
-    uninstall
-    ;;
-  purge)
-    uninstall
-    rm -vrf ~/.config/radion
-    ;;
-  *)
-    printf "USAGE: %s <command>\n" "$0"
-    printf "   install    installs radion\n"
-    printf "   uninstall  uninstalls the radion executables, ~/.config/mpv/scripts/icecast-logger.lua\n"
-    printf "   purge      completely uninstalls radion, including all radion config files\n"
-    ;;
+ install)
+  #make scripts executable, copy them to the path:
+  chmod +x radion.sh record-toggle.sh
+  cp radion.sh record-toggle.sh "$HOME"/.local/bin/
+  #Create necessary directories, copy files:
+  mkdir "$HOME"/.config/radion/
+  if [[ ! -d "$HOME"/Music/radion/ ]];then mkdir "$HOME"/Music/radion/ ;fi
+  cp stations.txt "$HOME"/.config/radion/
+  cp radion.conf "$HOME"/.config/radion/
+  cp icecast-logger.lua -t "$HOME"/.config/mpv/scripts/
+  cp radion-rofi-theme.rasi "$HOME"/.config/radion/
+  cp -r png/  "$HOME"/.config/radion/
+  ;;
+ uninstall)uninstall
+  ;;
+ purge)uninstall
+  rm -vrf "$HOME"/.config/radion
+  ;;
+ *)
+  printf "USAGE: %s <command>\n" "$0"
+  printf "   install   :installs radion\n"
+  printf "   uninstall :uninstalls the radion executables, lua \n"
+  printf "   purge     :completely uninstalls radion, including all radion config files\n"
+  ;;
 esac
 
 exit 0
